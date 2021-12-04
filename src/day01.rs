751069b24a050039e937fae4928a0900d041a1f1
TobiasForner <t.forner@tum.de>
2021-12-05 00:36:49 +0100

    refactor day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,6 +1,5 @@
 use crate::{print_result, util::read_to_vec};
 use anyhow::Result;
-use itertools::Itertools;
 
 pub fn part1() -> Result<()> {
     let count = read_to_vec::<u32>("./inputs/day01.txt")?
@@ -11,17 +10,9 @@ pub fn part1() -> Result<()> {
 }
 
 pub fn part2() -> Result<()> {
-    let mut count: u32 = 0;
-    let mut last: u32 = u32::MAX;
-    let it = read_to_vec::<u32>("./inputs/day01.txt")?
-        .into_iter()
-        .tuple_windows();
-    for (x1, x2, x3) in it {
-        let value: u32 = x1 + x2 + x3;
-        if value > last {
-            count += 1;
-        }
-        last = value;
-    }
+    let count = read_to_vec::<u32>("./inputs/day01.txt")?
+        .windows(4)
+        .filter(|t| t[3] > t[0])
+        .count();
     print_result!(1, 2, count);
 }
