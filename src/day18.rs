1b1dabf8cbc01599a2244891d4cdb0f947db4297
TobiasForner <t.forner@tum.de>
2021-12-20 19:23:53 +0100

    refactor day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,10 +1,10 @@
 use crate::util::read_to_vec;
 use anyhow::{Error, Result};
-use std::cmp;
+use std::fmt::{self, Debug, Formatter};
 use std::ops::Add;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Clone)]
 enum PairNumber {
     Nested(Box<PairNumber>, Box<PairNumber>),
     Single(u32),
@@ -15,7 +15,6 @@ impl FromStr for PairNumber {
     fn from_str(s: &str) -> Result<Self> {
         let chars: Vec<char> = s.chars().collect();
         fn build_from_pos<'a>(pos: usize, chars: &Vec<char>) -> (usize, PairNumber) {
-            //println!("pos {}", pos);
             use PairNumber::*;
             if chars[pos + 1] == '[' {
                 let (left_end, left) = build_from_pos(pos + 1, chars);
@@ -55,181 +54,131 @@ impl FromStr for PairNumber {
     }
 }
 
+impl Debug for PairNumber {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        use PairNumber::*;
+        match self {
+            Single(v) => {
+                write!(f, "{}", v)
+            }
+            Nested(l, r) => {
+                write!(f, "[{:?}, {:?}]", l, r)
+            }
+        }
+    }
+}
+
 impl Add for PairNumber {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        let unreduced = PairNumber::Nested(Box::new(self), Box::new(other));
-        unreduced.reduce()
+        let mut unreduced = PairNumber::Nested(Box::new(self), Box::new(other));
+        unreduced.reduce();
+        unreduced
     }
 }
 
 impl PairNumber {
-    fn reduce(self) -> Self {
-        let mut res = self;
+    fn reduce(&mut self) {
         loop {
-            let (found, _, _, _, _, new_res) = res.reduce_explode(0);
-            res = new_res;
+            let (found, _, _, _, _) = self.reduce_explode(0);
             if found {
                 continue;
             }
-            let (found, new_res) = res.reduce_split();
-            res = new_res;
-            if !found {
+            if !self.reduce_split() {
                 break;
             }
         }
-        //println!("{:?}", res);
-        res
     }
 
-    fn reduce_explode(self, level: u32) -> (bool, bool, bool, u32, u32, PairNumber) {
+    fn reduce_explode(&mut self, level: u32) -> (bool, bool, bool, u32, u32) {
         use PairNumber::*;
-        //(found, left_done, right_done, left, right, self)
+        //(found, left_done, right_done, left_v, right_v)
         match self {
-            Nested(x, y) => {
-                let x = *x;
-                let y = *y;
-                match (&x, &y) {
-                    (Single(v1), Single(v2)) => {
-                        if level >= 4 {
-                            (true, false, false, *v1, *v2, Single(0))
-                        } else {
-                            (
-                                false,
-                                false,
-                                false,
-                                0,
-                                0,
-                                Nested(Box::new(Single(*v1)), Box::new(Single(*v2))),
-                            )
-                        }
+            Nested(x, y) => match (&**x, &**y) {
+                (Single(v1), Single(v2)) => {
+                    if level >= 4 {
+                        let v1 = *v1;
+                        let v2 = *v2;
+                        *self = Single(0);
+                        (true, false, false, v1, v2)
+                    } else {
+                        (false, false, false, 0, 0)
                     }
-                    (Single(v1), Nested(_, _)) => {
-                        let (found, left_done, right_done, left, right, new_right) =
+                }
+                (Single(_), Nested(_, _)) => {
+                    let (found, left_done, right_done, left, right) = y.reduce_explode(level + 1);
+                    if found && !left_done {
+                        x.add_right(left);
+                        (found, true, right_done, left, right)
+                    } else {
+                        (found, left_done, right_done, left, right)
+                    }
+                }
+                (Nested(_, _), Single(_)) => {
+                    let (found, left_done, right_done, left, right) = x.reduce_explode(level + 1);
+
+                    if found && !right_done {
+                        y.add_left(right);
+                        (found, left_done, true, left, right)
+                    } else {
+                        (found, left_done, right_done, left, right)
+                    }
+                }
+                (Nested(_, _), Nested(_, _)) => {
+                    let (found, left_done, right_done, left, right) = x.reduce_explode(level + 1);
+                    if found {
+                        if !right_done {
+                            y.add_left(right);
+                        }
+                        (found, left_done, true, left, right)
+                    } else {
+                        let (found, left_done, right_done, left, right) =
                             y.reduce_explode(level + 1);
                         if found && !left_done {
-                            (
-                                found,
-                                true,
-                                right_done,
-                                left,
-                                right,
-                                Nested(Box::new(Single(v1 + left)), Box::new(new_right)),
-                            )
+                            x.add_right(left);
+                            (found, true, right_done, left, right)
                         } else {
-                            (
-                                found,
-                                left_done,
-                                right_done,
-                                left,
-                                right,
-                                Nested(Box::new(Single(*v1)), Box::new(new_right)),
-                            )
-                        }
-                    }
-                    (Nested(_, _), Single(v2)) => {
-                        let (found, left_done, right_done, left, right, new_left) =
-                            x.reduce_explode(level + 1);
-                        if found && !right_done {
-                            (
-                                found,
-                                left_done,
-                                true,
-                                left,
-                                right,
-                                Nested(Box::new(new_left), Box::new(Single(v2 + right))),
-                            )
-                        } else {
-                            (
-                                found,
-                                left_done,
-                                right_done,
-                                left,
-                                right,
-                                Nested(Box::new(new_left), Box::new(Single(*v2))),
-                            )
-                        }
-                    }
-                    (Nested(_, _), Nested(_, _)) => {
-                        let (found, left_done, right_done, left, right, mut new_left) =
-                            x.reduce_explode(level + 1);
-                        let mut new_right = y;
-                        if found {
-                            if !right_done {
-                                new_right = new_right.add_left(right);
-                            }
-                            (
-                                found,
-                                left_done,
-                                true,
-                                left,
-                                right,
-                                Nested(Box::new(new_left), Box::new(new_right)),
-                            )
-                        } else {
-                            let (found, left_done, right_done, left, right, new_right) =
-                                new_right.reduce_explode(level + 1);
-                            if !left_done {
-                                new_left = new_left.add_right(left);
-                            }
-                            (
-                                found,
-                                true,
-                                right_done,
-                                left,
-                                right,
-                                Nested(Box::new(new_left), Box::new(new_right)),
-                            )
+                            (found, left_done, right_done, left, right)
                         }
                     }
                 }
-            }
-            Single(_) => (false, false, false, 0, 0, self),
+            },
+            Single(_) => (false, false, false, 0, 0),
         }
     }
 
-    fn reduce_split(self) -> (bool, Self) {
+    fn reduce_split(&mut self) -> bool {
         use PairNumber::*;
         match self {
-            Nested(x, y) => {
-                let (found, new_left) = x.reduce_split();
-                if found {
-                    (found, Nested(Box::new(new_left), y))
-                } else {
-                    let (found, new_right) = y.reduce_split();
-                    (found, Nested(Box::new(new_left), Box::new(new_right)))
-                }
-            }
+            Nested(x, y) => x.reduce_split() || y.reduce_split(),
             Single(v) => {
-                if v >= 10 {
-                    let half = (v as f32) / 2 as f32;
-                    (
-                        true,
-                        Nested(
-                            Box::new(Single(half.floor() as u32)),
-                            Box::new(Single(half.ceil() as u32)),
-                        ),
-                    )
+                if *v >= 10 {
+                    let half = (*v as f32) / 2 as f32;
+                    *self = Nested(
+                        Box::new(Single(half.floor() as u32)),
+                        Box::new(Single(half.ceil() as u32)),
+                    );
+                    true
                 } else {
-                    (false, Single(v))
+                    false
                 }
             }
         }
     }
 
-    fn add_right(self, val: u32) -> Self {
+    fn add_right(&mut self, val: u32) {
         use PairNumber::*;
         match self {
-            Nested(x, y) => Nested(x, Box::new(y.add_right(val))),
-            Single(v) => Single(v + val),
+            Nested(_, y) => y.add_right(val),
+            Single(v) => *v += val,
         }
     }
 
-    fn add_left(self, val: u32) -> Self {
+    fn add_left(&mut self, val: u32) {
         use PairNumber::*;
         match self {
-            Nested(x, y) => Nested(Box::new(x.add_left(val)), y),
-            Single(v) => Single(v + val),
+            Nested(x, _) => x.add_left(val),
+            Single(v) => *v += val,
         }
     }
 
@@ -253,30 +202,18 @@ pub fn part1(path: &str) -> Result<u32> {
     Ok(res)
 }
 
-fn largest_sum(path: &str, reverse: bool) -> Result<u32> {
-    let nested_number_pairs: Vec<PairNumber> = read_to_vec(path)?;
-    let max_len = nested_number_pairs.len();
-    let mut best = 0;
-    for first in 0..max_len {
-        for second in first..max_len - 1 {
-            let mut nested_number_pairs: Vec<PairNumber> = read_to_vec(path)?;
-            let x = nested_number_pairs.remove(first);
-            let y = nested_number_pairs.remove(second);
-            let res = if reverse {
-                (y + x).magnitude()
-            } else {
-                (x + y).magnitude()
-            };
-            if res > best {
-                best = res;
-            }
-        }
-    }
-    Ok(best)
+fn largest_sum(path: &str) -> Result<u32> {
+    let nums: Vec<PairNumber> = read_to_vec(path)?;
+    let mut max = 0;
+    nums.iter()
+        .flat_map(|num| std::iter::repeat(num).zip(nums.iter()))
+        .for_each(|(n1, n2)| {
+            max = max.max(n1.clone().add(n2.clone()).magnitude());
+            max = max.max(n2.clone().add(n1.clone()).magnitude());
+        });
+    Ok(max)
 }
 pub fn part2(path: &str) -> Result<u32> {
-    let x = largest_sum(path, false)?;
-    let y = largest_sum(path, true)?;
-
-    Ok(cmp::max(x, y))
+    let x = largest_sum(path)?;
+    Ok(x)
 }
