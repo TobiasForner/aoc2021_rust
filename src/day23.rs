6e99bda199543d61b2c76de51d8d48d0efd8536e
Tobias Forner <t.forner@tum.de>
2021-12-26 02:44:41 +0100

    add optimiztions

--- src/day23.rs
+++ src/day23.rs
@@ -53,66 +53,77 @@ fn solve_stack(
     ));
     let mut res = usize::MAX;
     'outer: while !stack.is_empty() {
-        let (hallway, room1, room2, room3, room4, cost) = stack.pop().unwrap();
+        let (hallway, room1, room2, room3, room4, mut cost) = stack.pop().unwrap();
 
         //check if solved
-        if hallway.iter().all(|x| *x == '.')
-            && room1.iter().all(|x| *x == 'A')
-            && room2.iter().all(|x| *x == 'B')
-            && room3.iter().all(|x| *x == 'C')
-            && room4.iter().all(|x| *x == 'D')
-        {
+        if solved(&hallway, &room1, &room2, &room3, &room4) {
             if res > cost {
                 res = cost;
             }
             continue;
         }
-        let mut new_cost = cost;
+        if cost > res {
+            continue;
+        }
         let hallway_positions: Vec<usize> = vec![0, 1, 3, 5, 7, 9, 10];
         //try to move from the hallway into correct room
-        for (pos, c) in hallway.iter().enumerate() {
-            let c = *c;
-            if c == '.' {
-                continue;
-            }
-            let (front_pos, costs, mut dest_room) = match c {
-                'A' => (2, 1, room1.clone()),
-                'B' => (4, 10, room2.clone()),
-                'C' => (6, 100, room3.clone()),
-                'D' => (8, 1000, room4.clone()),
-                _ => panic!("Invalid char {}", c),
-            };
-            let room_enter = dest_room.iter().all(|x| *x == '.' || *x == c);
-
-            if room_enter {
-                let mut success = false;
-                if pos < front_pos && (pos + 1..=front_pos).all(|x| hallway[x] == '.') {
-                    new_cost +=
-                        costs * (front_pos - pos + dest_room.iter().filter(|x| **x == '.').count());
-                    success = true;
-                } else if pos >= front_pos && (front_pos..=pos - 1).all(|x| hallway[x] == '.') {
-                    new_cost +=
-                        costs * (pos - front_pos + dest_room.iter().filter(|x| **x == '.').count());
-                    success = true;
+        let mut change = true;
+        while change {
+            change = false;
+            for (pos, c) in hallway.iter().enumerate() {
+                let c = *c;
+                if c == '.' {
+                    continue;
                 }
-                if success {
-                    //println!("moved into correct room!");
-                    let mut hallway = hallway.clone();
-                    let place = (0..room_size)
-                        .filter(|x| dest_room[*x as usize] == '.')
-                        .max()
-                        .unwrap();
-                    dest_room[place] = c;
-                    hallway[pos] = '.';
-                    let rooms = match c {
-                        'A' => (dest_room, room2.clone(), room3.clone(), room4.clone()),
-                        'B' => (room1.clone(), dest_room, room3.clone(), room4.clone()),
-                        'C' => (room1.clone(), room2.clone(), dest_room, room4.clone()),
-                        'D' => (room1.clone(), room2.clone(), room3.clone(), dest_room),
-                        _ => panic!("Invalid char {}", c),
-                    };
-                    stack.push((hallway, rooms.0, rooms.1, rooms.2, rooms.3, new_cost));
-                    continue 'outer;
+                let (front_pos, costs, mut dest_room) = match c {
+                    'A' => (2, 1, room1.clone()),
+                    'B' => (4, 10, room2.clone()),
+                    'C' => (6, 100, room3.clone()),
+                    'D' => (8, 1000, room4.clone()),
+                    _ => panic!("Invalid char {}", c),
+                };
+                let room_enter = dest_room.iter().all(|x| *x == '.' || *x == c);
+
+                if room_enter {
+                    let mut success = false;
+                    if pos < front_pos && (pos + 1..=front_pos).all(|x| hallway[x] == '.') {
+                        cost += costs
+                            * (front_pos - pos + dest_room.iter().filter(|x| **x == '.').count());
+                        success = true;
+                    } else if pos >= front_pos && (front_pos..=pos - 1).all(|x| hallway[x] == '.') {
+                        cost += costs
+                            * (pos - front_pos + dest_room.iter().filter(|x| **x == '.').count());
+                        success = true;
+                    }
+                    if cost > res {
+                        continue;
+                    }
+                    if success {
+                        change = true;
+                        //println!("moved into correct room!");
+                        let mut hallway = hallway.clone();
+                        let place = (0..room_size)
+                            .filter(|x| dest_room[*x as usize] == '.')
+                            .max()
+                            .unwrap();
+                        dest_room[place] = c;
+                        hallway[pos] = '.';
+                        /*let rooms = match c {
+                            'A' => (dest_room, room2.clone(), room3.clone(), room4.clone()),
+                            'B' => (room1.clone(), dest_room, room3.clone(), room4.clone()),
+                            'C' => (room1.clone(), room2.clone(), dest_room, room4.clone()),
+                            'D' => (room1.clone(), room2.clone(), room3.clone(), dest_room),
+                            _ => panic!("Invalid char {}", c),
+                        };*/
+
+                        if solved(&hallway, &room1, &room2, &room3, &room4) {
+                            if res > cost {
+                                res = cost;
+                                println!("update");
+                            }
+                            continue 'outer;
+                        }
+                    }
                 }
             }
         }
@@ -124,7 +135,6 @@ fn solve_stack(
             (room3.clone(), 6, 'C'),
             (room4.clone(), 8, 'D'),
         ];
-        let new_res = usize::MAX;
         for (room, room_pos, c) in room_with_pos {
             let room_enter = room.iter().all(|x| *x == '.' || *x == c);
             if room_enter {
@@ -161,11 +171,17 @@ fn solve_stack(
                                     _ => panic!("Invalid char {}", c),
                                 };
                                 let new_cost = cost + costs * (pos + 1 + p - room_pos);
-                                /*let new_res =
-                                    solve(hallway, rooms.0, rooms.1, rooms.2, rooms.3, new_cost);
-                                if new_res < res {
-                                    res = new_res;
-                                }*/
+                                if new_cost > res {
+                                    continue;
+                                }
+                                if solved(&hallway, &rooms.0, &rooms.1, &rooms.2, &rooms.3) {
+                                    if new_cost < res {
+                                        res = new_cost;
+                                        println!("update");
+                                    }
+                                    continue;
+                                }
+
                                 stack.push((hallway, rooms.0, rooms.1, rooms.2, rooms.3, new_cost));
                             }
                         }
@@ -199,11 +215,16 @@ fn solve_stack(
                                     _ => panic!("Invalid char {}", c),
                                 };
                                 let new_cost = cost + costs * (pos + 1 + room_pos - p);
-                                /*let new_res =
-                                    solve(hallway, rooms.0, rooms.1, rooms.2, rooms.3, new_cost);
-                                if new_res < res {
-                                    res = new_res;
-                                }*/
+                                if new_cost > res {
+                                    continue;
+                                }
+                                if solved(&hallway, &rooms.0, &rooms.1, &rooms.2, &rooms.3) {
+                                    if new_cost < res {
+                                        res = new_cost;
+                                        println!("update");
+                                        continue;
+                                    }
+                                }
                                 stack.push((hallway, rooms.0, rooms.1, rooms.2, rooms.3, new_cost));
                             }
                         }
@@ -213,11 +234,22 @@ fn solve_stack(
                 }
             }
         }
-        if new_res < res {
-            res = new_res;
-        }
     }
     res
+}
+
+fn solved(
+    hallway: &Vec<char>,
+    room1: &Vec<char>,
+    room2: &Vec<char>,
+    room3: &Vec<char>,
+    room4: &Vec<char>,
+) -> bool {
+    hallway.iter().all(|x| *x == '.')
+        && room1.iter().all(|x| *x == 'A')
+        && room2.iter().all(|x| *x == 'B')
+        && room3.iter().all(|x| *x == 'C')
+        && room4.iter().all(|x| *x == 'D')
 }
 
 pub fn solve(
